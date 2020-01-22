795c84db459779a51c67ee305df825b157bfe436
juan uribe <juanpablouribe@mac.com>
2020-01-21 20:41:52 -0500

    added api routes for adding customer

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -45,12 +45,12 @@ module.exports = function(app) {
     });
   });
 
-  // // Create a new example
-  // app.post("/api/examples", function(req, res) {
-  //   db.Example.create(req.body).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
+  // Create a new customer
+  app.post("/api/customer", function(req, res) {
+    db.Customers.create(req.body).then(function(dbCustomers) {
+      res.json(dbCustomers);
+    });
+  });
 
   // // Delete an example by id
   // app.delete("/api/examples/:id", function(req, res) {
