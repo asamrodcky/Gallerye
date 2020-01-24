599fb171d09625a97c2df57f639a2c64aaca8af1
Etienne Warneck <etienne.warneck@gmail.com>
2020-01-24 14:49:52 -0500

    clean up artworks.js

--- public/js/artwork.js
+++ public/js/artwork.js
@@ -2,59 +2,53 @@ $(document).ready(function () {
 
     var dataArr = [];
 
-    // $('.card-img-top').on('click', function () {
-    //     $('#anchor').attr('href', '/views/product.html')
-    // })
-
     $.ajax({
         url: "/api/allArtwork",
         method: "GET",
 
     })
         .then(function (res) {
-            console.log(res)
+            // console.log(res)
             // console.log(res.length)
-            // dataArr = artData
-            console.log("Attached to artwork.html")
+            // console.log("Attached to artwork.html")
 
-            // For each piece that our server sends us back
+            // For each artwork that our server sends us back:
             for (var i = 0; i < res.length; i++) {
-                // Create a parent div to hold piece data
+
+                // New parent Div
                 var itemDiv = $("<div>");
-                // Add a class to this div
-                itemDiv.addClass("card");
-                // Add an id 
-                itemDiv.attr("id", "artpiece-" + i);
-                // Append the item to the container section
+                // Add a class and id to the Div and set id 
+                itemDiv.addClass("card").attr("id", "artpiece-" + i);
+                // Append the item to the container in artwork.html
                 $(".artworkDiv").append(itemDiv);
-                // Now  we add our book data to the well we just placed on the page
-                var img = $("<img class='card-img-top' src=${} alt='Card image cap'>")
 
-                $("#artpiece-" + i).append(img)
-
-
-                // cardDiv.prepend('<img id="theImg" src=' + res[i].img  + '/>')
-
+                //New children Div
                 var cardDiv = $("<div>")
+                //add a class
                 cardDiv.addClass("card-body")
-                // cardDiv.append(`<img class='card-img-top' src=${products.imgUrl} alt='product.title'>`)
-                cardDiv.append("<a id='anchor' href='/product/:id'> <img class='card-img-top' src='images/IMG_001.jpg'  alt='image' /></a>")
-                // <img src="<%= product.imageUrl %>" alt="<%= product.title %>">
+                //create <a><img><a/>
+                var anchorAndImg = $("<a id='anchor' href='/product/:id'> <img class='card-img-top' src='images/IMG_001.jpg' alt='card image'> </a>")
+                //append <a><img><a/> for each item
+                $("#artpiece-" + i).append(anchorAndImg);
+                //append Name, Year, Artist, Price, button
                 cardDiv.append("<p id='card-title'>" + res[i].product_name + " (" + res[i].year + ")" + "</p>")
                 cardDiv.append("<p id='card-text-artist'>" + res[i].artist + "</p>")
                 cardDiv.append("<p id='card-text-price'>$" + res[i].price + "</p>")
-                // cardDiv.append("<a href='#' class='btn btn-primary'>Go somewhere</a>")
-
+                // cardDiv.append("<a href='#' class='btn btn-primary'>Buy</a>")
+                //Append carDiv to itemDiv
                 itemDiv.append(cardDiv)
 
-                $(".container").append(itemDiv);
+                //Console log attirbutes for each anchor in main container 
+                const DivForCards = document.querySelector(".artworkDiv a");
+                console.log("attributesTest", DivForCards.attributes);
             }
         })
         .catch(err => {
             console.log(err)
         });
 
-   
+
 
 });
+
 
