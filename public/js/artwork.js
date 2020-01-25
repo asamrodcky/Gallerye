b5e6c2bf924ef8df40d477e0901149c47addf0b4
samrod826 <samrodriguez826@gmail.com>
2020-01-25 16:07:36 -0500

    trying to draw info from db for product page

--- public/js/artwork.js
+++ public/js/artwork.js
@@ -9,16 +9,16 @@ $(document).ready(function () {
         method: "GET",
     })
         .then(function (res) {
+            console.log(res)
             var prices = ["Low to High", "High to Low"]
             var years = ["Oldest to Newest", "Newest to Oldest"]
 
             // For each artwork that our server sends us back:
             for (var i = 0; i < res.length; i++) {
-
                 // NEW PARENT DIV
                 var itemDiv = $("<div>");
                 // Add a class and id to the Div and set id 
-                itemDiv.addClass("card").attr("id", "artpiece-" + i);
+                itemDiv.addClass("card").attr("id", "artpiece-" + res[i].id);
 
                 // Append parent div to container in artwork.html
                 $(".artworkDiv").append(itemDiv);
@@ -28,9 +28,9 @@ $(document).ready(function () {
                 //add a class
                 cardDiv.addClass("card-body")
                 //create <a><img><a/>
-                var anchorAndImg = $("<a id='anchor' href='/product/"+ i +"'> <img class='card-img-top' id='artpiece-img-" + i + "' src='../images/IMG_"+ i +".jpg'alt='card image'> </a>")
+                var anchorAndImg = $("<a id='anchor' href='/product/"+ res[i].id +"'> <img class='card-img-top' id='artpiece-img-" + res[i].id + "' src='../images/IMG_"+ res[i].id +".jpg'alt='card image'> </a>")
                 //append <a><img><a/> for each item
-                $("#artpiece-" + i).append(anchorAndImg);
+                $("#artpiece-" + res[i].id).append(anchorAndImg);
                 //append Name, Year, Artist, Price, button
                 cardDiv.append("<p id='card-title'>" + res[i].product_name + " (" + res[i].year + ")" + "</p>")
                 cardDiv.append("<p id='card-text-artist'>" + res[i].artist + "</p>")
