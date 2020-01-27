5adf04f670ce92bf74a2cf403623ae40aa348085
samrod826 <samrodriguez826@gmail.com>
2020-01-27 07:22:01 -0500

    nav bar changes on sign in

--- README.md
+++ README.md
@@ -2,16 +2,23 @@
 
 Problem:
 
-Creatives need a virtual space to display, buy, and sell artwork.
+Many a gallery site (e.g. Sotheby’s, Christie’s) are made for the artists that have already made it. Even the smaller galleries that may not be as prominent hold artists, and their works, of which they have already selected to represent. Creating art is both free and freeing, so why not allow any and every artist the opportunity to present, market, manage themselves as a professional. 
 
 Solution:
 
-Gallerye is an application that will help creatives display their artwork in a virtual gallery setting. Like any museum or gallery you have ever been to, art work is hung or installed for the public to view, learn about, and potentially purchase. Gallerye is briding the gap between the physical gallery and the virtual world.
+Gallerye is an open-source E-commerce art site that allows users to buy, sell, bid on artworks directly sourced from individual artists. It represents a virtual gallery that has infinite capacity and the ability for each artist to represent themselves as a professional in the creative market.
 
 Utilization:
 
-Creatives and customers will be able to view art for sale and purchase both and digital and physical pieces for themselves. Creatives will also be able to sell their art through the application. 
+Gallerye was created with the average creative in mind, a virtual gallery to present, buy and sell/auction art.
 
 The Future:
+
+* Ensuring that sign-in/login/logout works (as “when connected” / “when not connected”)
+* Allow for auction and bids on the site instead of flat-out buys
+* Allow artists to upload image files from their computer instead of providing a URL/image address (may need S3 bucket?)
+* Set up a messaging tool for user to user interaction
+* Paypal/Shopify or other E-commerce platform for payment
+* Potential stock price for individual pieces and artists (Stock X influenced???)
 
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
   //SCRIPT CAROUSEL
-  $(document).ready(function () {
-    // Activate Carousel
-    $("#carouselExampleSlidesOnly").carousel({ interval: 3500 });
+  // Activate Carousel
+  $("#carouselExampleSlidesOnly").carousel({ interval: 3500 });
 
-    // Enable Carousel Indicators
-    $(".item1").click(function () {
-      $("#myCarousel").carousel(0);
-    });
-    $(".item2").click(function () {
-      $("#myCarousel").carousel(1);
-    });
-    $(".item3").click(function () {
-      $("#myCarousel").carousel(2);
-    });
-
-    //SCRIPT MODAL
-    $("#sign").on('click', function () {
-      $('.modal').modal('show');
-    });
+  // Enable Carousel Indicators
+  $(".item1").click(function () {
+    $("#myCarousel").carousel(0);
   });
+  $(".item2").click(function () {
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function () {
+    $("#myCarousel").carousel(2);
+  });
+
+  //SCRIPT MODAL
+  $("#sign").on('click', function () {
+    $('.modal').modal('show');
+  });
+
 
   //Modal adds a customer to database
   $("#add-customer").on("click", function (event) {
     event.preventDefault();
+
     // Make a newBook object
     var newCustomer = {
       first_name: $("#firstname").val().trim(),
@@ -32,19 +32,67 @@ $(document).ready(function () {
       password: $("#password").val().trim()
 
     };
+
     // Send an AJAX POST-request with jQuery
     $.post("/api/customer", newCustomer)
       // On success, run the following code
-      .then(function (data) {
-        // Log the data we found
-        console.log(data);
+      .done(function() {
+        alert( "second success" );
+      })
+      .fail(function() {
+        alert( "error" );
+      })
+      .always(function() {
+        alert( "finished" );
       });
+
+    document.cookie = "email=" + newCustomer.email
+    document.cookie = "signedIn=true"
+
     // Empty each input box by replacing the value with an empty string
     $("#firstname").val("");
     $("#lastname").val("");
     $("#email").val("");
     $("#password").val("");
+
+    // Close modal after completion
+    $('.modal').modal('hide');
+
+    // Reload page
+    window.location.reload()
   });
+
+  if (document.cookie.split("; ")[1] === "signedIn=true") {
+    $(".navbar-nav").append(
+      `<li class='nav-item'>
+      <a class='nav-link' href='/sell'> Sell </a>
+      </li>
+      <li class='nav-item dropdown'>
+      <a class='nav-link dropdown-toggle' href='#' id='navbarDropdown'
+      data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'> Name </a>
+      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="/account">Account</a>
+        <a class="dropdown-item" id='logOut' href="/">Logout</a>
+      </div>
+      </li>`)
+
+    $("#logOut").on("click", function () {
+      alert("You have successfully logged out")
+      document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      document.cookie = "signedIn=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    })
+  }
+  else {
+    $(".navbar-nav").append(
+      `<li class='nav-item'>
+        <a class='nav-link' id='sign' href='#'>Sign Up/Log In</a>
+        </li>`)
+    $("#sign").on('click', function () {
+      $('.modal').modal('show');
+    });
+  }
+
+  console.log(document.cookie.split("; "))
 
 });
 
